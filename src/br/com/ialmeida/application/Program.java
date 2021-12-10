4f02cb008a9ad4c67c0d13d198bd8c68b72c15dc
Iuri Almeida <ialmeida@vertigo.com.br>
2021-12-10 17:43:21 -0300

    Criando a classe Position

---
+++ src/br/com/ialmeida/application/Program.java
@@ -0,0 +1,11 @@
+package br.com.ialmeida.application;
+
+import br.com.ialmeida.boardgame.Position;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Position position = new Position(2, 1);
+        System.out.println(position);
+    }
+}
