6ed1da8eed393fc151a7ae854548097cf86e68f1
Iuri Almeida <ialmeida@vertigo.com.br>
2021-12-10 17:45:42 -0300

    Implementando as classes Board e Piece

--- src/br/com/ialmeida/application/Program.java
+++ src/br/com/ialmeida/application/Program.java
@@ -1,11 +1,12 @@
 package br.com.ialmeida.application;
 
+import br.com.ialmeida.boardgame.Board;
 import br.com.ialmeida.boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
-        Position position = new Position(2, 1);
-        System.out.println(position);
+        Board board = new Board(2, 1);
+        System.out.println(board);
     }
 }
---
+++ src/br/com/ialmeida/boardgame/Piece.java
@@ -0,0 +1,15 @@
+package br.com.ialmeida.boardgame;
+
+public class Piece {
+    protected Position position;
+    private final Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        this.position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
