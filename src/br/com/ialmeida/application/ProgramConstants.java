6eb1448690a351ff00b326f74b997ae0933a9873
Iuri Almeida <ialmeida@vertigo.com.br>
2021-12-13 21:56:57 -0300

    Adicionando a nomenclatura  nas constantes do projeto

--- src/br/com/ialmeida/application/ProgramConstants.java
+++ src/br/com/ialmeida/application/ProgramConstants.java
@@ -3,22 +3,22 @@ package br.com.ialmeida.application;
 public class ProgramConstants {
 
     // Board
-    public static int ROWS = 10;
-    public static int COLUMNS = 10;
-    public static char FIRST_ROW = 'a';
-    public static char LAST_ROW = 'j';
+    public static final int ROWS = 10;
+    public static final int COLUMNS = 10;
+    public static final char FIRST_ROW = 'a';
+    public static final char LAST_ROW = 'j';
 
     // Game Colors
-    public static String COMPUTER_PIECE_COLOR = ANSIColorConstants.ANSI_YELLOW;
-    public static String PERSON_PIECE_COLOR = ANSIColorConstants.ANSI_PURPLE;
-    public static String INDICATORS_COLOR = ANSIColorConstants.ANSI_GREEN;
-    public static String RESET_COLOR = ANSIColorConstants.ANSI_RESET;
+    public static final String COMPUTER_PIECE_COLOR = ANSIColorConstants.ANSI_YELLOW;
+    public static final String PERSON_PIECE_COLOR = ANSIColorConstants.ANSI_PURPLE;
+    public static final String INDICATORS_COLOR = ANSIColorConstants.ANSI_GREEN;
+    public static final String RESET_COLOR = ANSIColorConstants.ANSI_RESET;
 
     // Game Numbers
-    public static int TOTAL_SUBMARINES = 10;
+    public static final int TOTAL_SUBMARINES = 10;
 
     // Game Names
-    public static String PLAYER = "JOGADOR";
-    public static String PLAYER_SETUP = "JOGADOR (SETUP)";
-    public static String COMPUTER = "COMPUTADOR";
+    public static final String PLAYER = "JOGADOR";
+    public static final String PLAYER_SETUP = "JOGADOR (SETUP)";
+    public static final String COMPUTER = "COMPUTADOR";
 }
